b51469be089beedfff73f81e954dedb2090d476d
Lovasoa <maitrejedi@lovasoa.fr>
2018-02-04 15:57:30 +0100

    Center the svg cursors

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <section class="container">
     <section>Last Projects</section>
     <section>About Me.</section>
-    <a>CLick me I'm Famous</a>
+    <a class="btn">CLick me I'm Famous</a>
     <section>
       <h4>Contact Form</h4>
     </section>
@@ -11,13 +11,6 @@
 </template>
 
 <style lang="sass" scoped>
-  a
-    text-decoration: underline
-    color: red
-    &:hover
-      color: white
-      background: mistyrose
-    &:active
-      color: green
-      border: 1px solid black
+  .container
+    margin-top: 50px
 </style>
