0c94a3480d6768113ae55acb62b5136af91018bb
Lovasoa <maitrejedi@lovasoa.fr>
2018-02-05 15:52:22 +0100

    update sever headers

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,7 +9,7 @@ module.exports = {
     },
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1, user-scalable=no' },
       { hid: 'description', name: 'description', content: 'Julie Perrot Portfolio' },
       { property: 'og:title', content: 'Julie Perrot' },
       { property: 'og:site_name', content: 'Julie Perrot' },
@@ -53,10 +53,5 @@ module.exports = {
   css: [
     '@/assets/sass/style.sass'
   ],
-  render: {
-    csp: {
-      enabled: true
-    }
-  },
   loading: false
 }
