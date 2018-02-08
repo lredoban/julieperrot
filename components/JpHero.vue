766603c9b60a839b7c569ce9492c4c338b2c153e
Lovasoa <maitrejedi@lovasoa.fr>
2018-02-08 17:09:05 +0100

    refine hero

--- components/JpHero.vue
+++ components/JpHero.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="hero">
+    <img :src="bgUrl" alt="Julie Perrot Hero">
     <h1>
       <slot>The h1 title</slot>
     </h1>
@@ -19,39 +20,20 @@ export default {
   @import "~assets/sass/helpers"
 
   #hero
-    --gradient-angle: 123deg
     position: relative
     margin-top: 50px
     width: 100vw
     box-sizing: border-box
     background-size: cover
     display: grid
-    align-items: end
-    height: 154px
-    padding: 20px
-    padding-bottom: 32px
-    background-image: var(--bg-image, none)
-    @media #{$xsmall-up}
-      padding: 36px
-      padding-bottom: 52px
-      height: 235px
-    @media #{$small-up}
-      padding: 66px
-      padding-bottom: 82px
-      height: 375px
-    @media #{$medium-up}
-      padding: 96px
-      padding-bottom: 102px
-      height: 454px
-    @media #{$large-up}
-      padding: 126px
-      padding-bottom: 162px
-      height: 704px
-    @media #{$xlarge-up}
-      padding: 206px
-      padding-bottom: 219px
-      height: 939px
+    //background-image: var(--bg-image, none)
     h1
+      position: absolute
       margin: 0
-      line-height: 1em
+      line-height: 1
+      left: 7vw
+      bottom: calc(23.8636364% - 5px)
+      @media #{$small-up}
+        left: 17vw
+        bottom: calc(23.8636364% - 8px)
 </style>
