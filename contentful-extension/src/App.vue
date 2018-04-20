74845569a5bf8a8c9f81f9963f084e2c7e4cad38
Lovasoa <maitrejedi@lovasoa.fr>
2018-04-20 13:51:15 -0700

    feat(contentful-extension): set the base for the extension

---
+++ contentful-extension/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <jp-image imgSrc="https://source.unsplash.com/featured/?cat,sport" svgType="levres" svgTop="left" rightGradient="1" bottomGradient="3"></jp-image>
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+import JpImage from '../../components/JpImage'
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome Smile!'
+    }
+  },
+  components: {
+    JpImage
+  }
+}
+</script>
+
+<style lang="scss">
+  #app {
+    max-width: 600px;
+    padding-top: 3em;
+    margin: 0 auto
+  }
+</style>
