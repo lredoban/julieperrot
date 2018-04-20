e9aad88295ec7d95161a96543fc7252ef42da939
Lovasoa <maitrejedi@lovasoa.fr>
2018-04-20 16:01:50 -0700

    feat(contentful-extension): retrieve linked asset

--- contentful-extension/src/App.vue
+++ contentful-extension/src/App.vue
@@ -1,29 +1,63 @@
 <template>
   <div id="app">
-    <jp-image imgSrc="https://source.unsplash.com/featured/?cat,sport" svgType="levres" svgTop="left" rightGradient="1" bottomGradient="3"></jp-image>
-    <h1>{{ msg }}</h1>
+    <jp-image
+      v-if="imgSrc"
+      :imgSrc="imgSrc"
+      :svgType="svgType"
+      :svgTop="svgTop"
+      :rightGradient="rightGradient"
+      :bottomGradient="bottomGradient"/>
   </div>
 </template>
 
 <script>
 import JpImage from '../../components/JpImage'
+//import cfExtension from 'contentful-ui-extensions-sdk'
+const cfExtension = require('contentful-ui-extensions-sdk')
 
 export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome Smile!'
+      imgSrc: false,
+      svgType: false,
+      svgTop: false,
+      rightGradient: false,
+      bottomGradient: false
     }
   },
   components: {
     JpImage
+  },
+  created () {
+    cfExtension.init((api) => {
+      console.warn('init')
+      const imageLink = api.entry.fields.image
+      const imageId = imageLink.getValue().sys.id
+      api.space.getAsset(imageId).then((data) => {
+        console.warn('yo', data)
+        this.imgSrc = data.fields.file['fr-FR'].url + '?w=270'
+      })
+
+      const {svgType, svgTop, rightGradient, bottomGradient} = api.field.getValue()
+      this.svgType = svgType
+      this.svgTop = svgTop
+      this.rightGradient = rightGradient
+      this.bottomGradient = bottomGradient
+
+      setTimeout(() => api.window.updateHeight(), 2000) //lancer après le chargement de l'image
+    })
+    // this.svgType = "levres"
+    // this.svgTop = "left"
+    // this.rightGradient = 5
+    // this.bottomGradient = 5
   }
 }
 </script>
 
 <style lang="scss">
   #app {
-    max-width: 600px;
+    max-width: 270px;
     padding-top: 3em;
     margin: 0 auto
   }
