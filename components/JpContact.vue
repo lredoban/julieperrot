ea6a3133d00de79892014689b07b4f916cfa42f6
Julie Perrot <iJulie@MacBook-Pro-de-Julie.local>
2018-05-24 23:08:28 +0200

    feat(form): last step before validation

--- components/JpContact.vue
+++ components/JpContact.vue
@@ -1,19 +1,24 @@
 <template>
   <section id="contact">
-    <form @submit.prevent="submit" rel="form">
+    <form v-if="step < 4" @submit.prevent="submit" rel="form">
       <div class="step" v-show="step === 1">
-        <textarea name="message" v-model="message" placeholder="Any message?"></textarea>
+        <textarea required name="message" v-model="message" rows="1" minlength="10" maxlength="280" placeholder="Any message?"></textarea>
         <button class="btn" @click.prevent="step = 2">Next</button>
       </div>
       <div class="step" v-show="step === 2">
-        <input type="textarea" name="name" id="name" placeholder="Ok. What is your name?" v-model="name">
+        <input required type="textarea" name="name" id="name" placeholder="Ok. What is your name?" v-model="name">
         <button class="btn" @click.prevent="step = 3">Next</button>
+        <button class="btn-prev" @click.prevent="step = 1">← Prev</button>
       </div>
       <div class="step" v-show="step === 3">
-        <input type="email" name="email" id="email" placeholder="Your email?" v-model="email">
+        <input required type="email" name="email" id="email" placeholder="Your email?" v-model="email">
         <button type="submit" class="btn">Submit!</button>
+        <button class="btn-prev" @click.prevent="step = 2">← Prev</button>
       </div>
     </form>
+    <div v-else>
+      <img :src="responseGif" alt="contact gif" style="max-width: 500px">
+    </div>
   </section>
 </template>
 
@@ -43,11 +48,22 @@ export default {
         body: this.encode({'form-name': 'contact', ...formData})
       })
       .then(rep => {
+        this.step = 4
         console.log('good', rep)
       })
       .catch(err => {
+        this.step = 5
         console.log('ugly', err)
       })
+    }
+  },
+  computed: {
+    responseGif() {
+      if (this.step < 4) {
+        return ''
+      }
+      return this.step === 4 ? 'https://media.giphy.com/media/YcMs3OGd89Pxu/giphy.gif'
+        : 'https://media.giphy.com/media/nLhdSinRtaL2E/giphy.gif'
     }
   }
 }
@@ -83,4 +99,10 @@ export default {
     > *:first-child
       width: 100%
   
+  .btn-prev
+    background: none
+    border: none
+    text-transform: lowercase
+    cursor: pointer
+    text-align: left
 </style>
