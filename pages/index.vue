852b1d17889da1bf2d310f26db2cf621469c13a5
Julie Perrot <iJulie@MacBook-Pro-de-Julie.local>
2018-06-15 00:13:37 +0200

    feat(homepage): contentfullize hero text

--- pages/index.vue
+++ pages/index.vue
@@ -13,7 +13,7 @@
       </nav>
       <div class="presentation">
         <h1 class="h2">French Photo- grapher</h1>
-        <p>Look, just because I don't be givin' no man a foot massage don't make it right for Marsellus to throw Antwone into a glass motherfuckin' house, fuckin' up the way the nigger talks. Motherfucker do that shit to me, he better paralyze my ass, 'cause I'll kill the motherfucker, know what I'm sayin'?</p>
+        <div v-html="homePage.heroText"></div>
       </div>
     </section>
     <section>
